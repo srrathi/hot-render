380e26b31a18bd4b984f8b6eee194eb5edfd4f5e
Sitaram Rathi <srrathi2000@gmail.com>
2024-04-13 19:19:37 +0530

    added discord webhook

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const data = require('./URL.json')
 
+const URL = "https://dc-bot-01s9.onrender.com/discord"
+
 const sendRequests = async () => {
     try {
         const promisesArray = data.map(url => fetch(url));
         const resp = await Promise.all(promisesArray);
-        console.log(resp?.map(r => r?.status))
+
+        const message = data.map((url, idx) => `${url}: status ${resp[idx]}`).join("\n")
+        const options = {
+            method: 'POST',
+            body: JSON.stringify({
+                title: 'Render Hot Cron',
+                description: message,
+                webhookUrl: process.env.WEBHOOK_URL
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            }
+        }
+        fetch(URL, options);
     } catch (err) {
         console.log("error occurred in script", err)
     }
